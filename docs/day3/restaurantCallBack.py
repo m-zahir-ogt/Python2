94caf51815c00878d27135ec048ab0914caf2281
Mehmet.Zahir <m.zahir@gmail.com>
2025-10-07 12:07:44 +0200

    restaurantCallBack

---
+++ docs/day3/restaurantCallBack.py
@@ -0,0 +1,17 @@
+# it is a callBack example. custumer in restaurant put order and water deliver food
+def orderFood(foof_name:str):
+    print(f"your {food_name} deliver soon")
+
+def checkFoodExistance(foof_name:str):
+#        0    1   2     ....n
+     arr_foods =["Pizza", "Pasta", "Dönner"]
+     arr_length = len(arr_foods)
+     for i in range(arr_length)
+     
+     print(f"we have {arr_length} foods in Menü")
+          
+
+
+    #-- actusl program use upper defined methods --#
+    order_string = input("what is your order?")
+    orderFood(order_string)
